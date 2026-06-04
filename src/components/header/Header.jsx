4cd028fdfb502febe12a8e4454b5e35fd6b965c1
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-04 14:35:04 +0530

    Added profile img

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
+import './Header.css'
+import profileImg from '../../assets/profile-img.jpg'
+
 export default function Header() {
     return(
         <header>
-            <img src="" />
+            <img className='profile-img' src={profileImg} />
+            <div>
+                <div>
+                    <h1>Sam Smith</h1>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </header>        
     )
 }
