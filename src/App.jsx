88d697064f603d8b41b4cf26fc0e7b30683af09d
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-04 14:03:15 +0530

    Updated File Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
+import Header from "./components/header/Header"
+import Main from "./components/Main/Main"
 import './App.css'
 
 export default function App() {
