b70194c58c1a8e4e9ad093c87dca399f9c0a73b4
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-04 15:58:58 +0530

    Footer design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header.jsx"
 import Main from "./components/Main/Main.jsx"
+import Footer from "./components/footer/Footer.jsx"
 import './App.css'
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <div className="container">
         <Header />
         <Main />
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <i class="fa-brands fa-square-x-twitter fa-2xl"></i>
+            <i class="fa-brands fa-square-facebook fa-2xl"></i>
+            <i class="fa-brands fa-square-instagram fa-2xl"></i>
+            <i class="fa-brands fa-square-github fa-2xl"></i>
+        </footer>
+    )
+}
